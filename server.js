9a7c8608fa6a045c2ba407f49bf87f044993c7f9
Ryan Christiani <ryan@ryanchristiani.com>
2015-10-14 22:14:08 -0400

    Adds message if not a GET request

--- server.js
+++ server.js
@@ -3,37 +3,41 @@ var queryString = require('querystring');
 var request = require('request');
 
 var server = http.createServer((req,res) => {
+	res.setHeader('Access-Control-Allow-Origin', '*'); 
+	res.setHeader("Content-Type", "application/json");
+	if(req.method === 'GET') {
 
-	var query = queryString.parse(req.url.substring(2));
-	var url = query.reqUrl;
-	var params = (function(queryObj) {
-		var obj = {};
-		for(key in queryObj) {
-			if(key !== 'reqUrl') {
-				var newKey = key.substring(7, (key.length - 1) );
-				obj[newKey] = queryObj[key];
+		var query = queryString.parse(req.url.substring(2));
+		var url = query.reqUrl;
+		var params = (function(queryObj) {
+			var obj = {};
+			for(key in queryObj) {
+				if(key !== 'reqUrl') {
+					var newKey = key.substring(7, (key.length - 1) );
+					obj[newKey] = queryObj[key];
+				}
 			}
-		}
-		return obj;
-	})(query);
+			return obj;
+		})(query);
 
-	var data = queryString.stringify(params);
+		var data = queryString.stringify(params);
 
 
-	request.get(url + '/?' + data ,(err,response,body) => {
+		request.get(url + '/?' + data ,(err,response,body) => {
 
-		res.setHeader('Access-Control-Allow-Origin', '*'); 
-		res.setHeader("Content-Type", "application/json");
-
-		if(response.statusCode === 200) {
-			res.writeHead(200);
-			res.end(body);
-		}
-		else {
-			res.writeHead(400);
-			res.end(body);
-		}
-	});
+			if(response.statusCode === 200) {
+				res.writeHead(200);
+				res.end(body);
+			}
+			else {
+				res.writeHead(400);
+				res.end(body);
+			}
+		});
+	}
+	else {
+		res.end('{"error": "Must be a GET request"}');
+	}
 });
 
 server.listen(4500);
