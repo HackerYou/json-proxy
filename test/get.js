88494916ef75777dbc8d158c61631352a9cd2093
Ryan Christiani <ryan@ryanchristiani.com>
2017-06-03 22:46:51 +0200

    Adding test that uses cache

--- test/get.js
+++ test/get.js
@@ -35,6 +35,25 @@ describe('Get requests', () => {
 				expect(res).to.be.an('object');
 				done();
 			});
-	})
+	});
+
+	it('should use the cache', (done) => {
+		request
+			.get('http://localhost:4500')
+			.send({
+				reqUrl: "https://www.goodreads.com/search/index.xml",
+				params: {
+					key: 'AYoaCzPGXisCTWsP6Ainw',
+					q: 'William Gibson'
+				},
+				xmlToJson: true,
+				useCache: true
+			})
+			.end((err,res) => {
+				expect(err).to.be(null);
+				expect(res).to.be.an('object');
+				done();
+			});
+	});
 });
 
